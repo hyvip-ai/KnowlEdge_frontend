8345af9775a9334721b14bf94c4d300b441ffa5f
hyvip-ai <rm2932002@gmail.com>
2023-11-01 16:04:22 +0530

    Update

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,18 @@
 export function Dashboard() {
   return (
     <div className='p-4'>
-      <h1 className='text-3xl text-primary font-bold '>
-        Haven't decided what should appear but something will appear soon enough
-      </h1>
+      <div>
+        <h1 className='text-2xl text-primary font-bold'>Dashboard</h1>
+        <h3 className='text-[16px] text-secondary font-semibold mt-2 mb-4'>
+          Currently we are not generating any stats (as I am a solo developer
+          working on it), but pretty soon we will have something (PROBABLY)
+        </h3>
+        <p className='text-secondary font-medium text-[14px]'>
+          But in case you want to feel like looking at dashboard here are some
+          pretty useless charts, show unemployment chart, or number of people
+          know react chart
+        </p>
+      </div>
     </div>
   );
 }
